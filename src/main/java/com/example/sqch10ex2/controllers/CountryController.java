0f4f4151c5738992cb102dfe6d77a9845e97b1b5
Artem Balan <a.s.balan96@gmail.com>
2024-01-30 22:51:06 +0200

    add another mapping as example of collection in response

--- src/main/java/com/example/sqch10ex2/controllers/CountryController.java
+++ src/main/java/com/example/sqch10ex2/controllers/CountryController.java
@@ -4,6 +4,8 @@ import com.example.sqch10ex2.model.Country;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CountryController {
 
@@ -11,5 +13,13 @@ public class CountryController {
     public Country france() {
         Country c = Country.of("France", 67);
         return c;
+    }
+
+    @GetMapping("/all")
+    public List<Country> countries() {
+        Country c1 = Country.of("France", 67);
+        Country c2 = Country.of("Spain", 47);
+
+        return List.of(c1, c2);
     }
 }
